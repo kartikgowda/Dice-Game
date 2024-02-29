4fa069b48df6a18edf2bdd5cdd0879f5c9ccdb07
Kartik <kartik.gowda55@gmail.com>
2024-02-29 11:44:29 +1100

    Add second dice and update image source

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let randomNumber1 = Math.ceil(Math.random() * 6);
+let randomNumber2 = Math.ceil(Math.random() * 6);
 
 let dicePlayerOne = document.querySelector('.img1');
+let dicePlayerTwo = document.querySelector('.img2');
 
 dicePlayerOne.setAttribute('src', `/images/dice${randomNumber1}.png`);
+
+dicePlayerTwo?.setAttribute('src', `images/dice${randomNumber2}.png`);
