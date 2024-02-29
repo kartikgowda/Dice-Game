f43e0c2db6a09dee5fb139eacca1c5593eec9ae8
Kartik <kartik.gowda55@gmail.com>
2024-02-29 11:55:16 +1100

    Update winner display in index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ dicePlayerOne.setAttribute('src', `/images/dice${randomNumber1}.png`);
 dicePlayerTwo?.setAttribute('src', `images/dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2) {
-  h1.textContent = 'Player One Wins!';
+  h1.textContent = '🏆 Player One Wins!';
 } else if (randomNumber2 > randomNumber1) {
-  h1.textContent = 'Player Two Wins!';
+  h1.textContent = 'Player Two Wins! 🏆';
 } else {
   h1.textContent = "It's a Draw!";
 }
